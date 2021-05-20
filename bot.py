8025fde8f26921bc7b3e500c33b4a40058e4165e
LoneCannibal <aakashsd2000@gmail.com>
2021-05-20 20:20:27 +0530

    Removed unnecessary comments

--- bot.py
+++ bot.py
@@ -21,18 +21,13 @@ def main():
                 if(tweets[i].in_reply_to_status_id!=None and tweets[i].id>latest_id): #Checking if the tweet in which the bot is tagged is a reply to another tweet or not AND checking if the tag request is duplicate or not
                     if(tweets[i].id>latest_id):
                         latest_id=tweets[i].id
-                        
                     parent_tweet=api.get_status(tweets[i].in_reply_to_status_id) #Get parent tweet
                     print("USER "+tweets[i].user.screen_name+" SENT A REQUEST")
                     print(parent_tweet.user.screen_name+": "+parent_tweet.text)
                     str="https://twitter.com/twitter/statuses/"+parent_tweet.id_str #Get tweet embed code
-                    #client = pdfcrowd.HtmlToImageClient('lonecannibal', pdfcrowd_api_key)
-                    #client.setOutputFormat('jpg')
-                    #client.convertUrlToFile(str, 'actualOutput.jpg')
                     html_code=(api.get_oembed(str)['html']) #Get the html code for the parent tweet
                     grabzIt.HTMLToImage(html_code) #Convert html to image
                     grabzIt.SaveTo("actualOutput.jpg") #Save the generated image to file
-                    #imgkit.from_url(str,'actualOutput.jpg',options={'javascript-delay':'4000', 'height':'600'}) #convert tweet to jpg
                     media = api.media_upload(filename='actualOutput.jpg') #create media object using jpg file
                     direct_message = api.send_direct_message(tweets[i].user.id, attachment_type='media', attachment_media_id=media.media_id,text="Here is the tweet that you requested! Tweet link: "+str)
                     str=""
