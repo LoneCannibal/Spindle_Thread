eeb3039fe7fe0457c6f52070bb7fa3f91dbe29be
LoneCannibal <aakashsd2000@gmail.com>
2021-05-23 15:43:55 +0530

    Fixed images not rendering

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ try:
 except:
     print("Keys file not found")
 
+
 pdf_path="actualOutput.pdf"
 img_path="actualOutput.jpg"
 random_file_name_length=10
@@ -21,7 +22,7 @@ grabzIt_key1 = keys[5]
 grabzIt_key2 = keys[6]
 dropbox_authkey = keys[7]
 
-def dropbox_upload():
+def dropbox_upload(): #Fucntion to upload PDF to Dropbox and generate the PDF link`
     dropbox_client = dropbox.Dropbox(dropbox_authkey)
     random_file_name = ''.join((random.choice(string.ascii_lowercase) for x in range(random_file_name_length)))
     dropbox_file_path = "/"+random_file_name+".pdf"
@@ -52,16 +53,20 @@ def main():
                     print(parent_tweet.user.screen_name+": "+parent_tweet.text)
                     str="https://twitter.com/twitter/statuses/"+parent_tweet.id_str #Get tweet embed code
                     html_code = (api.get_oembed(str)['html']) #Get the html code for the parent tweet
-                    options = GrabzItImageOptions.GrabzItImageOptions() #Options for Image
-                    options.browserWidth = 650 #Setting image width
-                    grabzIt.HTMLToImage(html_code,options) #Convert html to image
+                    image_options = GrabzItImageOptions.GrabzItImageOptions() #Options for Image
+                    image_options.browserWidth = 650 #Setting image width
+                    image_options.waitForElement = True #Telling the GrabzIt API to wait for css elements of the webpage to load
+                    grabzIt.HTMLToImage(html_code,image_options) #Convert html to image
                     grabzIt.SaveTo(img_path) #Save the generated image to file
-                    grabzIt.HTMLToPDF(html_code) #Convert HTML to PDF
+                    pdf_options = GrabzItPDFOptions.GrabzItPDFOptions() #Options for Image
+                    pdf_options.browserWidth = 650 #Setting browser width for the PDF
+                    pdf_options.waitForElement = True #Telling the GrabzIt API to wait for css elements of the webpage to load
+                    grabzIt.HTMLToPDF(html_code,pdf_options) #Convert HTML to PDF
                     grabzIt.SaveTo(pdf_path) #Save generated PDF
                     dropbox_output_link=dropbox_upload() #Function to upload PDF to  Dropbox and generate the link
                     media = api.media_upload(filename=img_path) #create media object using jpg file
-                    direct_message = api.send_direct_message(tweets[i].user.id, attachment_type='media', attachment_media_id=media.media_id,text="Here is the tweet that you requested! Tweet link: "+str)
-                    direct_message = api.send_direct_message(tweets[i].user.id, text="And here is a download link to a pdf copy of the tweeet! "+dropbox_output_link)
+                    direct_message = api.send_direct_message(tweets[i].user.id, attachment_type='media', attachment_media_id=media.media_id,text="Here is the tweet that you requested! Tweet link: "+str) #Send the image using direct message
+                    direct_message = api.send_direct_message(tweets[i].user.id, text="And here is a download link to a pdf copy of the tweeet! "+dropbox_output_link) #Send the PDF download link using direct message
                     str=""
             time.sleep(15)
     except Exception as e:
